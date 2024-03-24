503a6086e35d17e12e9903e90e8acf81a7f9f367
Alex Simonov <alex.simonof@gmail.com>
2024-03-24 16:38:36 +0200

    script

--- js/script.js
+++ js/script.js
@@ -57,41 +57,57 @@ const week = {
 // task 2
  
 
-const library = {
-  book: {
-    title: 'George Orwell',
-    autor: '1984',
-    year: '1949',
-  },
-  user: {
-    name: 'Victor',
-  },
 
-  addBook (title, autor, year){
 
-    return this.book = new Book(title, autor, year)
-    
-  },
 
-  removeBook(book) {
 
-  },
+function Library() {
 
+  this.books = [];
+  this.users = [];
+
+  this.addBook = function(book) {
+    this.books.push(book)
+  };
+  this.addUser = function(user) {
+    this.users.push(user)
+  };
+  this.searchByName = function(title) {
+    let bk = this.books.filter((el) => el.title.match(title));
+    return bk
+  };
 
 }
+
+function Book(title, autor, year, status, id){
+  this.title = title;
+  this.autor = autor;
+  this.year = year;
+  this.status = status;
+  this.id = getRandom(15);
+}
+
+function getRandom(num) {
+  return num = Math.round(Math.random()* num);
+}
+
+function User(name) {
+  this.name = name;
+}
+
+const library = new Library()
+library.addBook(new Book('1984', 'George Orwell', '1949', 'доступно'))
+library.addBook(new Book('На Западном фронте без перемен', 'Эрих Мария Ремарк', '1929', 'выдана'))
+library.addBook(new Book('Мастер и Маргарита', 'Михаил Булгаков', '1940', 'доступно'))
+
+library.addUser(new User('Alex'))
+library.addUser(new User('Artem'))
+library.addUser(new User('Sergey'))
+
+
+console.log(library)
+console.log(library.searchByName('1984'))
+
 // 
 
-library.addBook ('Hary Poter', 'Joanne Rowling', '1997')
-console.log(library)
 
-
-// library.addBook();
-
-// const user = new User("name");
-
-// library.removeBook(book);
-// user.borrowBook("title", library);
-// user.returnBook("title", library);
-// book.addRating("user", 5);
-
-//  = {title: 'Hary Poter', autor: 'Joanne Rowling', year: '1997'}
